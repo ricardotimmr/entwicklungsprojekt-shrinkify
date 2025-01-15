17ce66822a7dc1e679849bc00aac60da4941fb9b
Ricardo <139075806+ricardotimmr@users.noreply.github.com>
2025-01-15 16:56:16 +0100

    poc6: minor changes to lines of code for better readability

--- POCs/6-poc-alternative-upload-quellen/server.js
+++ POCs/6-poc-alternative-upload-quellen/server.js
@@ -53,7 +53,7 @@ app.post("/upload", upload.single("file"), (req, res) => {
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) {
         if (err.code === "LIMIT_FILE_SIZE") {
-            return res.status(413).json({ message: "File too large. Max 50 MB allowed." });
+            return res.status(413).json({ message: "File too large. Max 100 MB allowed." });
         }
     } else if (err) {
         return res.status(400).json({ message: err.message });
