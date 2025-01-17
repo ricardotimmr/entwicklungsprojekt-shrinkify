26cec060c30a30a5c637c24b48932a3af63ffe02
Ricardo <139075806+ricardotimmr@users.noreply.github.com>
2025-01-17 11:06:05 +0100

    doc: added folders for scripts and html

--- static/scripts/contentmanager.js
+++ static/scripts/contentmanager.js
@@ -1,3 +1,4 @@
+// Function to toggle the customer card
 function toggleCard(button) {
     var card = button.closest('.customer').querySelector('.customer-links');
 
@@ -10,18 +11,21 @@ function toggleCard(button) {
     }
 }
 
+// Function to add percentage symbol
 function addPercentageSymbol(input) {
     if (input.value && !input.value.trim().endsWith('%')) {
         input.value = input.value.trim() + '%';
     }
 }
 
+// Function to add MB
 function addMB(input) {
     if (input.value && !input.value.trim().endsWith('MB') && !input.value.trim().endsWith('mb')) {
         input.value = input.value.trim() + 'MB';
     }
 }
 
+// Function to add credits
 function addCredits(input) {
     if (input.value && !input.value.trim().endsWith('Credits')) {
         input.value = input.value.trim() + 'Credits';
