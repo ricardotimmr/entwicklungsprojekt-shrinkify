d1b3ab23020290546ad10acde23559504a490e85
Ricardo <139075806+ricardotimmr@users.noreply.github.com>
2025-01-18 15:37:58 +0100

    backend: minor code changes for backend/contentmanager-page

--- src/scripts/createNewCustomerLinks.js
+++ src/scripts/createNewCustomerLinks.js
@@ -11,27 +11,6 @@ function toggleCard(button) {
     }
 }
 
-// Function to add percentage symbol
-function addPercentageSymbol(input) {
-    if (input.value && !input.value.trim().endsWith('%')) {
-        input.value = input.value.trim() + '%';
-    }
-}
-
-// Function to add MB
-function addMB(input) {
-    if (input.value && !input.value.trim().endsWith('MB') && !input.value.trim().endsWith('mb')) {
-        input.value = input.value.trim() + 'MB';
-    }
-}
-
-// Function to add credits
-function addCredits(input) {
-    if (input.value && !input.value.trim().endsWith('Credits')) {
-        input.value = input.value.trim() + 'Credits';
-    }
-}
-
 // Links von der Datenbank abrufen
 function fetchLinks() {
     fetch("http://localhost:3000/links")
