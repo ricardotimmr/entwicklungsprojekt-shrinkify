3f996ebab739ca23048466441b4cc6c570fd5edd
cosimazink <cosima_hiromi.zink@smail.th-koeln.de>
2025-02-23 16:10:44 +0100

    feat: downloadlink in email

--- index.js
+++ index.js
@@ -538,6 +538,7 @@ app.get('/download-all/:cardId', (req, res) => {
     });
 });
 
+
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "✔️ Vorhanden" : "❌ Fehlt");
 
