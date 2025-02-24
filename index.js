6b6aef9251f5c68ba047d3b0c36977a94948d5c4
Ricardo <139075806+ricardotimmr@users.noreply.github.com>
2025-02-24 16:23:37 +0100

    Email Zugang

--- index.js
+++ index.js
@@ -623,8 +623,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: 'shrinkify.ep@gmail.com',
+    pass: 'visn wnmr oovo velx',
   },
 });
 
